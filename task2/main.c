68530ab8575bbc7b432b6ec475f85c40ec710989
root <root@WIN-OQN2KUP6C3L.localdomain>
2026-04-13 18:24:10 +0300

    Task #4 almost completed (no tests)

---
+++ task2/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+
+struct message_t {
+    int pid;
+    char msg[1024];
+};
+
+void* read_thread(void* data) {
+    FILE *fifoFile;
+
+    fifoFile = fopen(data, "w+b");
+
+    while (1) {
+        struct message_t msg;
+
+        fread(&msg, sizeof(msg), 1, fifoFile);
+        
+        if (msg.pid != getpid()) {
+            printf(">%s", msg.msg);
+        }
+        else {
+            // Так как FIFO файл один и не разделен на write и read,
+            // то процесс может забрать из очереди своё же сообщение.
+            // Поэтому при получении своего сообщения оно переотправляется.
+            fwrite(&msg, sizeof(msg), 1, fifoFile);
+            fflush(fifoFile);
+        }
+    }
+
+    pthread_exit(0);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Task #2 requries name of fifo file\n");
+        return 1;
+    }
+
+    char *fifoName = argv[1];
+
+    pthread_t r_thread;
+    pthread_create(&r_thread, NULL, read_thread, fifoName);
+
+    FILE *fifoFile;
+
+    fifoFile = fopen(fifoName, "wb");
+
+    if (fifoFile == NULL) {
+        fprintf(stderr, "Cannot open fifo file!");
+        return 1;
+    }
+
+    while (1) {
+        struct message_t msg;
+        msg.pid = getpid();
+
+        fgets(msg.msg, sizeof(msg.msg), stdin);
+
+        if (msg.msg == NULL) {
+            printf("Cannot read data from stdin!");
+            continue;
+        }
+        
+        char success = fwrite(&msg, sizeof(msg), 1, fifoFile);
+        if (!success) {
+            printf("Error occured while writing the message!");
+        }
+
+        fflush(fifoFile);
+    }
+
+    return 0;
+}
---
+++ task4/main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+
+#define MAX_ELBRUS_OPTS 6
+
+char is_elbrus_opt(char* opt) {
+    return strcmp(opt, "1c+") == 0 || strcmp(opt, "2c+") == 0 || strcmp(opt, "2c3") == 0 || strcmp(opt, "4c") == 0 || strcmp(opt, "8c") == 0 || strcmp(opt, "16c") == 0; 
+}
+
+char has_elbrus_opt(char** elbrus_opts, size_t elbrus_opts_size, char* opt) {
+    for (int i = 0; i < elbrus_opts_size; i++) {
+        if (strcmp(elbrus_opts[i], opt) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    char* elbrus_values[MAX_ELBRUS_OPTS];
+    size_t elbrus_value_size = 0;
+
+    const char* shortopts = "mcst";
+
+    const struct option longoptions[] = {
+        { "elbrus", required_argument, NULL, 'e' }
+    };
+
+    char result = 0;
+
+    opterr = 0;
+
+    char short_options[4];
+    size_t short_size = 0;
+
+    while ((result = getopt_long(argc, argv, shortopts, longoptions, NULL)) != -1) {
+        switch (result) {
+            case 'e':
+                if (!is_elbrus_opt(optarg)) {
+                    fprintf(stderr, "Incorrect option: 'elbrus=%s'\n", optarg);
+                    return 1;
+                }
+
+                if (has_elbrus_opt(elbrus_values, elbrus_value_size, optarg)) {
+                    continue;
+                }
+
+                elbrus_values[elbrus_value_size] = optarg;
+                elbrus_value_size++;
+                break;
+            case 'm':
+            case 'c':
+            case 's':
+            case 't':
+                short_options[short_size] = result;
+                short_size++;
+                break;
+            case '?':
+                fprintf(stderr, "Incorrect option: '%c'\n", optopt);
+                return 1;
+            default:
+                printf("%c\n", result);
+                break;
+        }
+    }
+
+    printf("Short options:");
+
+    if (short_size > 0) {
+        
+        for (int i = 0; i < short_size; i++) {
+            printf(" '%c'", short_options[i]);
+        }
+    }
+
+    printf("\nLong options:");
+
+    if (elbrus_value_size > 0) {
+        for (int i = 0; i < elbrus_value_size; i++) {
+            printf(" 'elbrus=%s'", elbrus_values[i]);
+        }
+    }
+
+    printf("\nNon options:");
+
+    if (optind < argc) {
+
+        for (int i = optind; i < argc; i++) {
+            printf(" '%s'", argv[i]);
+        }
+    }
+
+    putc('\n', stdout);
+    
+    return 0;
+}
