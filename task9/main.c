778ddd3056f7ce80f6dbb25c258ef361ab5d3e5c
root <root@WIN-OQN2KUP6C3L.localdomain>
2026-04-20 14:46:04 +0300

    Task #9 completed

---
+++ task9/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int size = 0;
+    int array[1024];
+    int input = 0;
+    char ch = 0;
+
+    while (1) {
+        int result = scanf("%d%c", &input, &ch);
+
+        if (result == EOF) {
+            break;
+        }
+
+        if (result == 0) {
+            fprintf(stderr, "Invalid input! Must be an array of integers, separated by whitespaces!\n");
+            return 1;
+        }
+
+        array[size] = input;
+        size++;
+
+        if (ch == '\n') {
+            break;
+        }
+    }
+
+    int maxI = 0, maxJ = 1;
+    int i = 0, j = 1;
+    while (j < size) {
+        if (maxJ - maxI < j - i) {
+            maxJ = j;
+            maxI = i;
+        }
+
+        if (array[j - 1] > array[j]) {
+            maxJ--;
+            i = j;
+        }
+
+        j++;
+    }
+
+    for (int i = maxI; i <= maxJ; i++) {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
